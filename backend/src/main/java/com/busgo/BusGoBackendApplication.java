89b0d4e15926c34ec895ff2134696e0760f2a3c9
Nguyen_Hong_Khanh <nhkyn.0410@gmail.com>
2026-01-11 14:43:13 +0700

    done setup basic maven

---
+++ backend/src/main/java/com/busgo/BusGoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.busgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusGoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusGoBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/busgo/BusGoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusGoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
